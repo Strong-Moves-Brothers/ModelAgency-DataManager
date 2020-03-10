1668689c73c248e56d800fe85630d640898c1f53
Vitor Matias Holzhausen <holzhausen.vitor@gmail.com>
2020-03-10 17:09:02 -0300

    factories introduced and implemented

--- App/Team.py
+++ App/Team.py
@@ -1,5 +1,63 @@
 from App.DateTreatment import DateTreatment
 from datetime import date
+from Tests.Exceptions import InvalidModelType, InvalidCourseType, InvalidShift, InvalidPaymentStatus
+
+
+class TeamFactory:
+
+    @classmethod
+    def factory(cls, team_course_type : str, team_shift: str, team_start_course_date : str):
+        team_start_course_date = DateTreatment.convert_string_to_date(team_start_course_date)
+        team_course_type = team_course_type.strip().upper()
+        team_shift = team_shift.strip().upper()
+        cls._input_verify(team_course_type, team_shift)
+        team_finish_course_date = DateTreatment.add_7_years(team_start_course_date)
+        team_name = cls._generate_team_name(team_start_course_date, team_course_type, team_shift).strip().upper()
+        team_id = cls._generate_team_id(team_start_course_date, team_shift)
+        return Team(team_name, team_course_type, team_shift, team_start_course_date, team_finish_course_date, team_id)
+
+    @staticmethod
+    def _input_verify(team_course_type : str, team_shift: str):
+        if team_shift not in ['MATUTINO', 'VESPERTINO', 'NOTURNO']:
+            raise InvalidShift('Turno inválido! Escolha entre: MATUTINO, VESPERTINO e NOTURNO.')
+        if team_course_type not in ['NORMAL', 'VIP']:
+            raise InvalidCourseType('Tipo de curso inválido! Escolha entre: NORMAL e VIP.')
+
+    @staticmethod
+    def _generate_team_id(team_start_course_date : date, team_shift : str):
+        if team_shift == 'MATUTINO':
+            t_f = 0
+        elif team_shift == 'VESPERTINO':
+            t_f = 1
+        else: #team_shift == 'NOTURNO'
+            t_f = 2
+        return int(f"{team_start_course_date.month}{team_start_course_date.year}{t_f}")
+
+    @staticmethod
+    def _generate_team_name(team_start_course_date : date, team_course_type : str, team_shift : str):
+        month = DateTreatment.months[team_start_course_date.month-1].upper()
+        return f"{month}_{team_start_course_date.year}_{team_course_type}_{team_shift}"
+
+class StudentFactory:
+
+    @classmethod
+    def factory(cls, student_name : str, student_age : str, student_type_m : str,
+                 student_is_graduated : bool, student_sit_pay_course : str):
+        student_name = student_name.strip().upper()
+        student_age = DateTreatment.convert_string_to_date(student_age)
+        student_is_graduated = int(student_is_graduated)
+        student_type_m = student_type_m.strip().upper()
+        student_sit_pay_course = student_sit_pay_course.strip().upper()
+        cls._input_verify(student_type_m, student_sit_pay_course)
+        return Student(student_name, student_age, student_type_m, student_is_graduated, student_sit_pay_course)
+
+    @staticmethod
+    def _input_verify(student_type_m, student_st_pay_course):
+        if student_type_m not in ['PASSARELA', 'LIMOUSINE', 'CIRCO']:
+            raise InvalidModelType('Tipo de modelo inválido. Por favor escolha entre: PASSARELA, LIMOUSINE E CIRCO.')
+        if student_st_pay_course not in ['PAGO', 'PENDENTE', 'ATRASADO']:
+            raise InvalidPaymentStatus(
+                'Situação do pagamento inválida. Por favor escolha entre: PAGO, PENDENTE E ATRASADO')
 
 
 class Team:
--- App/main.py
+++ App/main.py
@@ -1,4 +1,14 @@
 from App.Register import TeamRegister, StudentRegister
+from App.Team import TeamFactory, StudentFactory
 
-table_name = TeamRegister('VIP', 'NOTURNO', '06/04/2020').registrate()
-StudentRegister(table_name, 'Thammer Giovanni', '25-07-1997', 'PASSARELA', False, 'PAGO').registrate_into_team()
+def on_press1():
+    team = TeamFactory.factory('VIP', 'NOTURNO', '06/04/2020')
+    TeamRegister().registrate(team)
+    return team.name
+
+def on_press2(table):
+    student = StudentFactory.factory('Thammer Giovanni', '25-07-1997', 'PASSARELA', False, 'PAGO')
+    return StudentRegister(table).registrate(student)
+
+table = on_press1()
+on_press2(table)
