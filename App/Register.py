8fe13ffeabd364b6b1b35ef68472e8aa985636fa
Vitor Matias Holzhausen <holzhausen.vitor@gmail.com>
2020-03-09 22:53:47 -0300

    register modified

--- App/Register.py
+++ App/Register.py
@@ -1,6 +1,5 @@
 from App.Initializer import DatabaseInitializer
 
-
 class Register(DatabaseInitializer):
 
     def __init__(self, database: str, table: str):
@@ -10,9 +9,10 @@ class Register(DatabaseInitializer):
 
 
     def registrate_into_team(self, student):
-        insert_dt = f"INSERT INTO Student_db (name, age, type_m, is_graduated, sit_pay_curse, start_curse, finish_curse)"
-        " VALUES ('{student.name}','{student.age}','{student.type_m}','{student.is_graduated}','{student.sit_pay_curse}','{student.start_curse}'"
-        ",'{student.finish_curse}')"
+        insert_dt = f"INSERT INTO Student_db (name, age, type_m, is_graduated, sit_pay_curse, start_curse, " \
+                    f"finish_curse) VALUES ('{student.name}','{student.age}','{student.type_m}'," \
+                    f"'{student.is_graduated}','{student.sit_pay_curse}','{student.start_curse}'," \
+                    f"'{student.finish_curse}')"
 
         self.cursor.execute(insert_dt)
         self.conexao.commit()
@@ -25,6 +25,8 @@ class Creator(DatabaseInitializer):
         super().__init__()
 
     def create_table(self, team):
-        insert_dt = f"create table tal {team.team_id}"
+        insert_dt = f"CREATE TABLE {team.name} (name VARCHAR(255), age DATE, type_m VARCHAR(255), " \
+                    f"is_graduated BOOLEAN, sit_pay_curse VARCHAR(255), start_curse DATE, " \
+                    f"finish_curse DATE);"
         self.cursor.execute(insert_dt)
         self.conexao.commit()
