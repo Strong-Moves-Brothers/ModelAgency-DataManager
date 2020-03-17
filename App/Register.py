b76499cba8d9ce9d87793f4dd798f10f0306b1f5
Vitor Matias Holzhausen <holzhausen.vitor@gmail.com>
2020-03-17 16:41:14 -0300

    DataBaseRegisterMenu Added, databaseinitialization is dynamic now

--- App/Register.py
+++ App/Register.py
@@ -1,41 +1,41 @@
 from App.Initializer import DatabaseInitializer
+from Tests.Exceptions import TableRegisterError
 
 
-class StudentRegister(DatabaseInitializer):
+class StudentRegister:
     """
     This class contains the methods responsible for register an student in a specific Team Table
     """
-    def __init__(self):
+    def __init__(self, database : DatabaseInitializer):
         """
         This method instantiates a StudentRegister object and initializes the database connection
-        :param table: It's the team/table name
         """
-        super().__init__()
+        self.database = database
 
     def registrate(self, student, table):
         """
         This method is responsible for registrating the given student into the table specified by the
         table parameter of the object
         :param student: A Student object
+        :param table: It's the team/table name
         """
         insert_dt = f"INSERT INTO {table} (name, birth_date, model_type, is_graduated, sit_pay_curse" \
                     f") VALUES ('{student.name}', '{student.birth_date}', '{student.model_type}'," \
                     f" '{student.is_graduated}', '{student.sit_pay_curse}')"
         print(insert_dt)
-        self.cursor.execute(insert_dt)
-        self.conexao.commit()
-        self.conexao.close()
+        self.database.cursor.execute(insert_dt)
+        self.database.conn.commit()
 
 
-class TeamRegister(DatabaseInitializer):
+class TeamRegister:
     """
     This class contains the methods responsible for registrating a team by creating a table based on it's parameters.
     """
-    def __init__(self):
+    def __init__(self, database : DatabaseInitializer):
         """
         This method instantiates an TeamRegister object and initializes the database connection
         """
-        super().__init__()
+        self.database = database
 
     def registrate(self, team):
         """
@@ -48,6 +48,11 @@ class TeamRegister(DatabaseInitializer):
                     f"DEFAULT '{team.course_start_date}', finish_course_date DATE DEFAULT " \
                     f"'{team.course_finish_date}');"
         print(insert_dt)
-        self.cursor.execute(insert_dt)
-        self.conexao.commit()
-        self.conexao.close()
+        try:
+            self.database.cursor.execute(insert_dt)
+            print(insert_dt)
+        except:
+            self.database.conn.commit()
+            self.database.conn.close()
+            raise TableRegisterError()
+        self.database.conn.commit()
